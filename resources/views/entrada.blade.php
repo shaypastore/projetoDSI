e767c8470875d52ab4cb91ec5407495b5b60bdb8
Shaiane Pastore <shaiane.270193@bento.local>
2022-05-12 20:02:15 -0300

    Criação da página inicial

---
+++ resources/views/entrada.blade.php
@@ -0,0 +1,8 @@
+@extends('templates/layout')
+
+@section('titulo', 'Página inicial')
+
+@section('corpo')
+    <h1>Página inicial do site legal</h1>
+    <p>Você está entrando no site legal</p>
+@endsection
